a01393ff5c4514c95843b9f57d7cf8aef3f76495
alexis <alexisbuckens@gmail.com>
2021-09-04 23:32:54 +0200

    lexicon based sentiment analysis on religious texts

--- sentiment_analysis/SentimentAnalysis.py
+++ sentiment_analysis/SentimentAnalysis.py
@@ -1,4 +1,5 @@
-#from nltk.stem import PorterStemmer
+from textblob import TextBlob
+from nltk.stem import PorterStemmer
 from nltk.stem.wordnet import WordNetLemmatizer
 import matplotlib.pyplot as plt
 from wordcloud import WordCloud
@@ -6,24 +7,28 @@ import pandas as pd
 import numpy as np
 import os
 from nltk import RegexpTokenizer, word_tokenize, corpus, pos_tag
+from nltk.corpus import wordnet
 import nltk
 #nltk.download('averaged_perceptron_tagger')
 nltk.download('wordnet')
+#from transformers import BertTokenizer, TFBertForSequenceClassification
+#from transformers import InputExample, InputFeatures
 
 class Sentiment:
-    def __init__(self):
+    def __init__(self, lemmatization:bool = True):
+        self.lemmatization = lemmatization
+        self.stemmer = PorterStemmer()
+        self.lem = WordNetLemmatizer()
         self.texts_name = ["bible", "upanishads", "quran", "tao", "sumerian", "vedanta"]
         self.stop_words = corpus.stopwords.words('english')
         new_stopwords = ["thee", "ye"]
         self.stop_words.extend(new_stopwords) 
         self.current_dir =  os.path.dirname( os.path.abspath(__file__) )
-        #self.stemmer = PorterStemmer()
-        self.lem = WordNetLemmatizer()
 
     def analize_texts(self):
         text_dict = self.open_data()
         text_dict = self.dataprep(text_dict)
-
+        self.lexicon_classifier(text_dict)
     def open_data(self):
         
         text_dict = {}
@@ -35,6 +40,7 @@ class Sentiment:
         return text_dict
 
     def dataprep(self, text_dict:dict):
+        stemmed_dict = {}
         for k, text in text_dict.items():
             tokenizer = RegexpTokenizer(r'\w+')
 
@@ -49,11 +55,16 @@ class Sentiment:
             img.to_file("{}/images/{}_worcloud.jpeg".format(self.current_dir, k)) # example of something you can do with the img
             
             #pos tagging for further lemmatization
-            #words_stems = [self.stemmer.stem(word) for word in words_tokens]
-            pos_tagged_tokens = pos_tag(words_tokens)
-            #pos_tagged_tokens = [pos_tag(word) for word in words_tokens]
-            words_stems = [self.lem.lemmatize(word, self.get_wordnet_pos(tag)) for word, tag in pos_tagged_tokens]
-
+            if self.lemmatization:
+                pos_tagged_tokens = pos_tag(words_tokens)
+                words_stems = [self.lem.lemmatize(word, self.get_wordnet_pos(tag)) 
+                        for word, tag in pos_tagged_tokens if self.get_wordnet_pos(tag)]
+                missing = 100*(len(words_tokens)-len(words_stems))/len(words_tokens)
+                print("Words not lemmatized : {}%".format(missing))
+            else : 
+                words_stems = [self.stemmer.stem(word) for word in words_tokens]
+            stemmed_dict.update({k:words_stems})
+        return stemmed_dict
 
     def get_wordnet_pos(self, treebank_tag):
         if treebank_tag.startswith('J'):
@@ -67,7 +78,35 @@ class Sentiment:
         else:
             return None
 
+    def lexicon_classifier(self, stem_dict):
+        polarity_plot = {}
+        subj_plot = {}
+        for k, stems in stem_dict.items():
+            result = TextBlob(" ".join(stems)).sentiment.polarity
+            polarity_plot.update({k:result})
+            self.plot_evolution(k, stems)
+        
+        plt.bar(range(len(polarity_plot)), list(polarity_plot.values()), align='center')
+        plt.xticks(range(len(polarity_plot)), list(polarity_plot.keys()))
+        plt.savefig("{}/images/polarity_lexicon.jpeg".format(self.current_dir))
 
+    def plot_evolution(self, k, stems):
+        list_polarity = [TextBlob(word).sentiment.polarity for word in stems]
+
+        #Let's make a moving average so it's less of a mess
+        window_size = 300
+        windows = pd.Series(list_polarity).rolling(window_size)
+        moving_averages = windows.mean()
+
+        moving_averages_list = moving_averages.tolist()
+        list_polarity = moving_averages_list[window_size - 1:]
+        size = range(0, len(list_polarity))
+        
+        fig1, ax1 = plt.subplots()
+        ax1.plot(size, list_polarity)
+        fig1.savefig("{}/images/{}_evolution.jpeg".format(self.current_dir, k))
+        plt.close(fig1)
+        
 
 
 
